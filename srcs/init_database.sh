52cc13d65efd3cb5cf7895f27648753dff75aa18
ybaquet <ymontbrun@free.fr>
2021-01-27 11:32:11 +0100

    Ajout "$AUTOINDEX" dans init_database.sh

--- srcs/init_database.sh
+++ srcs/init_database.sh
@@ -7,3 +7,10 @@ echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+
+if [ "$AUTOINDEX" = "OFF" ]
+then
+	chmod +w  /etc/nginx/sites-available/default
+	sed -i 's/autoindex on;/autoindex off;/g' /etc/nginx/sites-available/default
+fi
+
